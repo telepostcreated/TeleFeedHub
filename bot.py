d2a46d40eb1717d99d733bfb6b8bbf688a990255
telepostcreated <isadoreschmelerxyzp@hotmail.com>
2026-07-19 16:51:46 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ from aiogram.filters import CommandStart
 from aiogram.types import Message, CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = ("8814954306:AAFpwg1Kbp38LUYt4FAYXKgPMkdfLuLpVfk")
 
-bot = Bot(8814954306:AAFpwg1Kbp38LUYt4FAYXKgPMkdfLuLpVfk)
+bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
 user_channels = {}
